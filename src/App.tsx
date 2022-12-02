832f96c2273bbc3f6bc6944c99efde572dd60549
IgorMazhitov <101721767+IgorMazhitov@users.noreply.github.com>
2022-12-02 14:49:54 +0300

    movies fetching during first loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from 'react';
 import MoviesList from './components/MoviesList/MoviesList';
 import SideBar from './components/SideBar/SideBar';
+import { useAppDispatch } from './rtk/hooks/hooks';
+import { getMovies, setLoadingState } from './rtk/slices/movieSlice';
 
 function App() {
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+
+    const params = {
+      country: 'us',
+      service: 'netflix',
+      type: 'movie',
+      page:  1,
+      output_language: 'en',
+      language: 'en'
+    }
+    
+    dispatch(setLoadingState(true))
+    dispatch(getMovies(params))
+  }, [])
 
   return (
     <div className="App relative bg-slate-900 w-screen h-screen overflow-scroll flex flex-row justify-start items-start m-0 p-0">
